0a939685bdd57b42385aac144a48821ae07ba4c9
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2019-09-23 09:02:50 +0200

    Now test app reports time taken.

--- Test.hx
+++ Test.hx
@@ -4,6 +4,9 @@ import connect.TileActivation;
 
 class Test {
     public static function main() {
+        // Get initial time
+        var initialTime = Date.now().getTime();
+
         // Load test config
         Config.load("test_config.json");
         
@@ -12,5 +15,10 @@ class Test {
             trace(request.id + " : " + request.asset.connection.id);
             return new TileActivation("# Hello, world!");
         }).process();
+
+        // Get final time
+        var finalTime = Date.now().getTime();
+
+        trace("Total time: " + ((finalTime - initialTime) / 1000) + " secs.");
     }
 }
