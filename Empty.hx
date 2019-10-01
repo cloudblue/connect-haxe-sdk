8deea94990645837081545fe5339028baa34a8ba
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2019-10-01 17:29:30 +0200

    Added all models to library.

--- Empty.hx
+++ Empty.hx
@@ -1,5 +1,45 @@
 import connect.Defaults;
-import connect.models.*;
+import connect.models.Account;
+import connect.models.Activation;
+import connect.models.Agreement;
+import connect.models.AgreementStats;
+import connect.models.Asset;
+import connect.models.Category;
+import connect.models.Choice;
+import connect.models.Configuration;
+import connect.models.Configurations;
+import connect.models.Connection;
+import connect.models.Constraints;
+import connect.models.Contact;
+import connect.models.ContactInfo;
+import connect.models.Contract;
+import connect.models.Conversation;
+import connect.models.Country;
+import connect.models.CustomerUiSettings;
+import connect.models.Document;
+import connect.models.DownloadLink;
+import connect.models.Event;
+import connect.models.Events;
+import connect.models.ExtIdHub;
+import connect.models.Family;
+import connect.models.Fulfillment;
+import connect.models.Hub;
+import connect.models.HubStats;
+import connect.models.IdModel;
+import connect.models.Instance;
+import connect.models.Item;
+import connect.models.Marketplace;
+import connect.models.Message;
+import connect.models.Model;
+import connect.models.Param;
+import connect.models.PhoneNumber;
+import connect.models.Product;
+import connect.models.ProductStats;
+import connect.models.ProductStatsInfo;
+import connect.models.Renewal;
+import connect.models.TierAccount;
+import connect.models.Tiers;
+import connect.models.User;
 
 class Empty {
     public static function main(): Void {
