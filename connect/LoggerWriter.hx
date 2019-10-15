f8ad64fd72de30b48acf83ff7187a2a0bea84b11
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2019-10-15 10:42:49 +0200

    Added docs to LoggerWriter class.

--- connect/LoggerWriter.hx
+++ connect/LoggerWriter.hx
@@ -1,26 +1,42 @@
 package connect;
 
 
+/**
+    Represents the default LoggerWriter. The `Logger` uses an instance of this class
+    to write log messages (by default to the standard output and the given filename).
+
+    The filename can be null if you do not want to write messages to a persistent file.
+**/
 class LoggerWriter {
-    public function setFilename(filename: String): Void {
-        this.filename = filename;
+    public function new() {
+        this.filename = null;
+        this.file = null;
     }
 
 
+    /**
+        Sets the filename of the log file if it has not been previously set.
+    **/
+    public function setFilename(filename: String): Void {
+        if (this.filename == null) {
+            this.filename = filename;
+        }
+    }
+
+
+    /** @returns The filename of the log file. **/
     public function getFilename(): String {
         return this.filename;
     }
 
 
+    /** Writes a line to the log output. The line feed character is added by the method. **/
     public function writeLine(line: String): Void {
         if (this.getFile() != null) {
             this.getFile().writeString(line + '\r\n');
         }
         Sys.println(line);
     }
-
-
-    public function new() {}
 
 
     private var filename: String;
