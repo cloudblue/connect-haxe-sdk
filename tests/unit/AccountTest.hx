11fa0b43b8f3cb0fc5456b63abde690a83a43280
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2019-11-08 14:53:56 +0100

    Update AccountTest.hx

--- tests/unit/AccountTest.hx
+++ tests/unit/AccountTest.hx
@@ -19,17 +19,17 @@ class AccountTest extends haxe.unit.TestCase {
 
     public function testList() {
         // Check accounts list
-        var accounts = Account.list(null);
+        final accounts = Account.list(null);
         assertTrue(Std.is(accounts, Collection));
         assertEquals(1, accounts.length());
 
         // Check first account
-        var account = accounts.get(0);
+        final account = accounts.get(0);
         assertTrue(Std.is(account, Account));
         assertEquals('VA-044-420', account.id);
 
         // Check mocks
-        var apiMock = cast(Env.getGeneralApi(), Mock);
+        final apiMock = cast(Env.getGeneralApi(), Mock);
         assertEquals(1, apiMock.callCount('listAccounts'));
         assertEquals(
             [null].toString(),
@@ -39,13 +39,13 @@ class AccountTest extends haxe.unit.TestCase {
 
     public function testCreate() {
         // Check account
-        var account = Account.create();
+        final account = Account.create();
         assertTrue(account != null);
         assertTrue(Std.is(account, Account));
         assertEquals('VA-044-420', account.id);
 
         // Check mocks
-        var apiMock = cast(Env.getGeneralApi(), Mock);
+        final apiMock = cast(Env.getGeneralApi(), Mock);
         assertEquals(1, apiMock.callCount('createAccount'));
         assertEquals(
             [].toString(),
@@ -55,7 +55,7 @@ class AccountTest extends haxe.unit.TestCase {
 
     public function testGetOk() {
         // Check subject
-        var account = Account.get('VA-044-420');
+        final account = Account.get('VA-044-420');
         assertTrue(account != null);
         assertTrue(Std.is(account, Account));
         assertTrue(Std.is(account.id, String));
@@ -83,7 +83,7 @@ class AccountTest extends haxe.unit.TestCase {
         assertEquals(false, account.sourcing);
 
         // Check mocks
-        var apiMock = cast(Env.getGeneralApi(), Mock);
+        final apiMock = cast(Env.getGeneralApi(), Mock);
         assertEquals(1, apiMock.callCount('getAccount'));
         assertEquals(
             ['VA-044-420'].toString(),
@@ -93,11 +93,11 @@ class AccountTest extends haxe.unit.TestCase {
 
     public function testGetKo() {
         // Check subject
-        var account = Account.get('VA-XXX-XXX');
+        final account = Account.get('VA-XXX-XXX');
         assertTrue(account == null);
 
         // Check mocks
-        var apiMock = cast(Env.getGeneralApi(), Mock);
+        final apiMock = cast(Env.getGeneralApi(), Mock);
         assertEquals(1, apiMock.callCount('getAccount'));
         assertEquals(
             ['VA-XXX-XXX'].toString(),
@@ -106,19 +106,19 @@ class AccountTest extends haxe.unit.TestCase {
 
 
     public function testListUsers() {
-        var users = Account.get('VA-044-420').listUsers();
+        final users = Account.get('VA-044-420').listUsers();
         assertTrue(users != null);
         assertTrue(Std.is(users, Collection));
         assertEquals(1, users.length());
 
-        var user = users.get(0);
+        final user = users.get(0);
         assertTrue(Std.is(user, User));
         assertEquals('UR-460-012-274', user.id);
     }
 
 
     public function testGetUserOk() {
-        var user = Account.get('VA-044-420').getUser('UR-460-012-274');
+        final user = Account.get('VA-044-420').getUser('UR-460-012-274');
         assertTrue(user != null);
         assertTrue(Std.is(user, User));
         assertEquals('UR-460-012-274', user.id);
@@ -126,7 +126,7 @@ class AccountTest extends haxe.unit.TestCase {
 
 
     public function testGetUserKo() {
-        var user = Account.get('VA-044-420').getUser('UR-XXX-XXX-XXX');
+        final user = Account.get('VA-044-420').getUser('UR-XXX-XXX-XXX');
         assertTrue(user == null);
     }
 }
