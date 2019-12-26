f13c09950ea1dbceb287f4d674e81c7b91ca3a9d
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2019-12-26 13:16:41 +0100

    .

--- connect/api/impl/ApiClientImpl.hx
+++ connect/api/impl/ApiClientImpl.hx
@@ -1,3 +1,7 @@
+/*
+    This file is part of the Ingram Micro CloudBlue Connect SDK.
+    Copyright (c) 2019 Ingram Micro. All Rights Reserved.
+*/
 package connect.api.impl;
 
 import connect.logger.ILoggerFormatter;
