437582cc80d0013ae2927b7ea1c272a1e24fb2b8
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2019-12-30 11:28:32 +0100

    Update before_script.sh

--- stuff/before_script.sh
+++ stuff/before_script.sh
@@ -2,9 +2,9 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   mkdir -p _build/java
   cd _build/java
-  git clone https://github.com/JaviCerveraIngram/connect-java-sdk.git .
+  git clone https://github.com/cloudblue/connect-java-sdk.git .
   git remote rm origin
-  git remote add origin https://JaviCerveraIngram:${doc_token}@github.com/JaviCerveraIngram/connect-java-sdk.git
+  git remote add origin https://cloudblue:${doc_token}@github.com/cloudblue/connect-java-sdk.git
   cd ../..
   cp stuff/gitignore_java _build/java/.gitignore
 else
