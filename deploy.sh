c750e5c2646d7f867147623b593735c2641b4bfd
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-03 14:01:54 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 cd `dirname $0`
 
-echo "Deploying Java package to Nexus repository using Maven..."
 mvn gpg:sign-and-deploy-file \
     -DpomFile=stuff/pom.xml \
     -Dfile=_build/java/Packager.jar \
     -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
     -DrepositoryId=maven-central
-
-echo "Done."
