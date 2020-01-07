f6660923911bbd9f87f90d00d1e2b8e858c28b95
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-07 16:10:27 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ mvn gpg:sign-and-deploy-file \
     -DpomFile=stuff/pom.xml \
     -Dfile=_build/java/Packager.jar \
     -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
-    -DrepositoryId=maven-central
+    -DrepositoryId=maven-central \
     -Dgpg.passphrase=passphrase
