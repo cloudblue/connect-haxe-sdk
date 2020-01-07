fd975302c04b13032d03cc209e630b84b614eb34
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-07 12:21:15 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd `dirname $0`
+
+mvn gpg:sign-and-deploy-file \
+    -DpomFile=stuff/pom.xml \
+    -Dfile=_build/java/Packager.jar \
+    -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+    -DrepositoryId=maven-central
