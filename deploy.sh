28dd4f34d4eab3cf4ec5479ae418d8f53982ef54
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-07 18:57:21 +0100

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-
-mvn gpg:sign-and-deploy-file \
-    -DpomFile=stuff/pom.xml \
-    -Dfile=_build/java/Packager.jar \
-    -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
-    -DrepositoryId=maven-central \
-    -Dgpg.passphrase=1234
