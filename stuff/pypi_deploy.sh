21f681b9d98ee153048d8d761a32b83fc464d6d1
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-08 15:53:00 +0100

    Update pypi_deploy.sh

--- stuff/pypi_deploy.sh
+++ stuff/pypi_deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sudo apt install python3-pip -y
-python3 --version
-pip --version
+pip3 --version
 # twine upload -u __token__ -p ${pypi_token} --repository-url https://test.pypi.org/legacy/ dist/*
