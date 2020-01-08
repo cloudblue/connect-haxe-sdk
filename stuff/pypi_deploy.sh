8a79ae4ad68ecb2895e7a27a2d7c6694e401968a
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-08 17:49:40 +0100

    Update pypi_deploy.sh

--- stuff/pypi_deploy.sh
+++ stuff/pypi_deploy.sh
@@ -2,9 +2,7 @@
 sudo apt install python3-setuptools python3-wheel twine -y
 cd _packages/connect.py
 python3 setup.py sdist bdist_wheel
-export TWINE_USERNAME=__token__
-export TWINE_PASSWORD=${pypi_token}
-export TWINE_REPOSITORY_URL=https://test.pypi.org/legacy/
-export TWINE_NON_INTERACTIVE
-twine upload dist/*
+echo "******** print .pypirc"
+cat ~/.pypirc
+twine upload -u __token__ -p ${pypi_token} -r https://test.pypi.org/legacy/ dist/*
 cd ../..
