07733ba839ccacc909a75962baa801b410291486
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-08 16:02:22 +0100

    Update pypi_deploy.sh

--- stuff/pypi_deploy.sh
+++ stuff/pypi_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt install python3-pip -y
-pip3 install setuptools twine wheel
+pip3 install setuptools
 # twine upload -u __token__ -p ${pypi_token} --repository-url https://test.pypi.org/legacy/ dist/*
