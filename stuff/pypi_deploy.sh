79f1565df849c98ba11a2a383eae640aea2fd981
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-08 18:04:42 +0100

    Update pypi_deploy.sh

--- stuff/pypi_deploy.sh
+++ stuff/pypi_deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 cp stuff/pypirc ~/.pypirc
 sed -i "s/__PYPI_TOKEN__/${pypi_token}/g" ~/.pypirc
-echo "******** print .pypirc"
-cat stuff/pypirc
-cat ~/.pypirc
 sudo apt install python3-setuptools python3-wheel twine -y
 cd _packages/connect.py
 python3 setup.py sdist bdist_wheel
