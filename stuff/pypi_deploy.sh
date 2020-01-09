0b9a572bfa1d0390f0f1d1da03d95bd00ce8a109
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-09 11:59:48 +0100

    Update pypi_deploy.sh

--- stuff/pypi_deploy.sh
+++ stuff/pypi_deploy.sh
@@ -4,11 +4,11 @@ sed -i "s/__PYPI_TOKEN__/${pypi_token}/g" ~/.pypirc
 sudo apt install python3-pip -y
 sudo -H pip3 install --upgrade pip
 echo "*** Installing setuptools..."
-sudo pip3 install setuptools
+sudo -H pip3 install setuptools
 echo "*** Installing wheel..."
-sudo pip3 install wheel
+sudo -H pip3 install wheel
 echo "*** Installing twine..."
-sudo pip3 install twine
+sudo -H pip3 install twine
 # sudo apt install python3-setuptools python3-wheel twine -y
 cd _build/python
 python3 setup.py sdist bdist_wheel
