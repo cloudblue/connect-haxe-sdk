2fb1f70f8ca7c56d41cebdef111cb39249eaa60c
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-09 09:49:47 +0100

    Python deployed to PyPI instead of TestPyPI

--- stuff/pypi_deploy.sh
+++ stuff/pypi_deploy.sh
@@ -4,5 +4,5 @@ sed -i "s/__PYPI_TOKEN__/${pypi_token}/g" ~/.pypirc
 sudo apt install python3-setuptools python3-wheel twine -y
 cd _build/python
 python3 setup.py sdist bdist_wheel
-twine upload -r testpypi dist/*
+twine upload -r pypi dist/*
 cd ../..
