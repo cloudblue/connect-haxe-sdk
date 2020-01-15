da50df66e16801f4c6176ca748518787ee789a76
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-15 10:09:19 +0100

    Travis script lists imported key IDs, to make sure they are correctly imported.

--- stuff/maven_deploy.sh
+++ stuff/maven_deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+
 export GPG_TTY=$(tty)
+
 echo "*** Decrypting and importing PGP key..."
 openssl aes-256-cbc -K $encrypted_6c315054a92d_key -iv $encrypted_6c315054a92d_iv -in stuff/key.gpg.enc -out stuff/key.gpg -d
 gpg --batch --passphrase ${mvn_passphrase} --import stuff/key.gpg
 rm stuff/key.gpg
+gpg --list-keys
+gpg --list-secret-keys
+
 echo "*** Deploying to Maven Central..."
 mvn gpg:sign-and-deploy-file \
   -DpomFile=stuff/pom.xml \
