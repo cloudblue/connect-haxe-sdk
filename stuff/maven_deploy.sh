5222600891b843775b9d24c33cbdaad6dd267984
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-15 13:17:47 +0100

    Update maven_deploy.sh

--- stuff/maven_deploy.sh
+++ stuff/maven_deploy.sh
@@ -14,6 +14,6 @@ mvn gpg:sign-and-deploy-file \
   -DpomFile=stuff/pom.xml \
   -Dfile=_build/java/Packager.jar \
   -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
-  -DrepositoryId=maven-central \
+  -DrepositoryId=maven-central-snapshots \
   -Dgpg.passphrase=${mvn_passphrase}
 echo "*** Done."
