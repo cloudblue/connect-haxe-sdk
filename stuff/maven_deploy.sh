85d7302336d987f73c8e4602dade340543924ecb
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-16 18:02:08 +0100

    Using deploy:deploy-file for deployment

--- stuff/maven_deploy.sh
+++ stuff/maven_deploy.sh
@@ -10,10 +10,10 @@ gpg --list-keys
 gpg --list-secret-keys
 
 echo "*** Deploying to Maven Central..."
-mvn gpg:sign-and-deploy-file \
+mvn deploy:deploy-file \
   -DpomFile=stuff/pom.xml \
   -Dfile=_build/java/Packager.jar \
-  -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+  -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 \
   -DrepositoryId=connect \
   -Dgpg.passphrase=${mvn_passphrase}
 echo "*** Done."
