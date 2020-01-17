8ae009e235ee3bdf482f94c9bf9185ebb07e209e
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-17 14:14:46 +0100

    Update maven_deploy.sh

--- stuff/maven_deploy.sh
+++ stuff/maven_deploy.sh
@@ -5,7 +5,7 @@ export GPG_TTY=$(tty)
 echo "*** Decrypting and importing PGP key..."
 #openssl aes-256-cbc -K $encrypted_6c315054a92d_key -iv $encrypted_6c315054a92d_iv -in stuff/key.gpg.enc -out stuff/key.gpg -d
 openssl aes-256-cbc -K $encrypted_4ac79ed20675_key -iv $encrypted_4ac79ed20675_iv -in stuff/key.gpg.enc -out stuff/key.gpg -d
-gpg --batch --passphrase ${mvn_passphrase} --import stuff/key.gpg
+#gpg --batch --passphrase ${mvn_passphrase} --import stuff/key.gpg
 rm stuff/key.gpg
 
 echo "*** Listing imported keys..."
