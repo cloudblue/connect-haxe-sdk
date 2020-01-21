4feb6a088a83bed0e2445a80fe104280f3f9229b
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-21 11:18:37 +0100

    Update deploy_maven_central.py

--- stuff/deploy_maven_central.py
+++ stuff/deploy_maven_central.py
@@ -1,9 +1,40 @@
+# This file is part of the Ingram Micro CloudBlue Connect SDK.
+# Copyright (c) 2019 Ingram Micro. All Rights Reserved.
+
 import os
 
-# def curl():
-#    import subprocess
+
+def curl(path: str, method: str, data: str) -> str:
+    import os
+    import subprocess
+    user = os.environ['mvn_user']
+    password = os.environ['mvn_password']
+    base_url = 'https://oss.sonatype.org/service/local/staging/profiles'
+    profile_id = 'JaviCerveraIngram'
+    args = [
+        'curl',
+        '-X', method.upper(),
+        '-d', data
+        '-u', '{}:{}'.format(user, password),
+        '-H', 'Content-Type:application/xml',
+        '{}/{}/{}'.format(base_url, profile_id, path)
+    ]
+    subprocess.run(args)  # , stdout=subprocess.PIPE)
+    return ''
+
+
+def start() -> str:
+    data = """
+    <promoteRequest>
+        <data>
+            <description>
+                connect.sdk upload
+            </description>
+        </data>
+    </promoteRequest>
+    """
+    return curl('start', 'post', data)
+
 
 if __name__ == '__main__':
-    print('cwd: ' + os.getcwd())
-    print('mvn_user: ' + os.environ['mvn_user'])
-    print('mvn_password: ' + os.environ['mvn_password'])
+    start()
