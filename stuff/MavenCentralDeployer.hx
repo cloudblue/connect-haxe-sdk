49c48aa4e10236a3145eec9d01e2522707bc2724
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-21 10:15:55 +0100

    .

--- stuff/MavenCentralDeployer.hx
+++ stuff/MavenCentralDeployer.hx
@@ -6,27 +6,26 @@
 
 class MavenCentralDeployer {
     public static function main(): Void {
-        haxe.Http.PROXY = {
-            port: 443,
-            host: '',
-            auth: {
-                user: Sys.getEnv('mvn_user'),
-                pass: Sys.getEnv('mvn_password')
-            }
-        };
         call('start', 'post');
     }
 
 
     private static function call(path: String, method: String, ?body: String): String {
-        final output = new haxe.io.BytesOutput();
-        final http = new haxe.Http(getUrl('start'));
-        if (body != null) {
-            http.setPostData(body);
-        }
+        final url = getUrl('start');
+        haxe.Http.PROXY = {
+            port: 443,
+            host: url,
+            auth: {
+                user: Sys.getEnv('mvn_user'),
+                pass: Sys.getEnv('mvn_password')
+            }
+        };
+        final http = new haxe.Http(url);
+        if (body != null) http.setPostData(body);
         var status = -1;
         http.onStatus = newStatus -> status = newStatus;
         http.onError = msg -> throw msg;
+        final output = new haxe.io.BytesOutput();
         http.customRequest(false, output, null, method.toUpperCase());
         while (status == null) {}
         return output.getBytes().toString();
