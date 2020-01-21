97b39dbcb721c6938cbd3e85bc52c681153b93cf
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-01-21 12:30:09 +0100

    Update deploy_maven_central.py

--- stuff/deploy_maven_central.py
+++ stuff/deploy_maven_central.py
@@ -1,22 +1,30 @@
 # This file is part of the Ingram Micro CloudBlue Connect SDK.
 # Copyright (c) 2019 Ingram Micro. All Rights Reserved.
 
-def curl(path: str, method: str, data: str, content_type: str = 'application/xml') -> str:
+profiles_url = 'https://oss.sonatype.org/service/local/staging/profiles'
+deploy_url = 'https://oss.sonatype.org/service/local/staging/deployByRepositoryId'
+profile_id = 'JaviCerveraIngram'
+
+
+def curl(url: str, method: str, data: str) -> str:
     import os
     import subprocess
     user = os.environ['mvn_user']
     password = os.environ['mvn_password']
-    base_url = 'https://oss.sonatype.org/service/local/staging/profiles'
-    profile_id = 'JaviCerveraIngram'
     args = [
         'curl',
         '-s',
-        '-X', method.upper(),
-        '-d', data,
         '-u', '{}:{}'.format(user, password),
-        '-H', 'Content-Type:{}'.format(content_type),
-        '{}/{}/{}'.format(base_url, profile_id, path)
     ]
+    
+    if method.lower() == '--upload-file':
+        args.extend(['-H', 'Content-Type:application/x-jar'])
+        args.extend(['--upload-file', data])
+    else:
+        args.extend(['-H', 'Content-Type:application/xml'])
+        args.extend(['-X', method.upper()])
+        args.extend(['-d', data])
+    args.append(url)
     return subprocess.run(args, stdout=subprocess.PIPE).stdout.decode('utf-8')
 
 
@@ -35,7 +43,7 @@ def start() -> str:
         </data>
     </promoteRequest>
     """
-    response = curl('start', 'post', data)
+    response = curl('{}/{}/start'.format(profiles_url, profile_id), 'post', data)
     root = parse_xml(response)
     if root.tag != 'nexus-error':
         return root \
@@ -65,10 +73,29 @@ def release() -> str:
 
 
 if __name__ == '__main__':
+    path = '_build/java'
+    files = [
+        'connect.sdk-18.0.jar',
+        'connect.sdk-18.0.pom',
+        'connect.sdk-18.0-sources.jar',
+        'connect.sdk-18.0-javadoc.jar'
+    ]
+
+    print('Starting repository...')
     repository_id = start()
-    upload(repository_id, '_build/java/connect.sdk-18.0.jar')
-    upload(repository_id, '_build/java/connect.sdk-18.0.pom')
-    upload(repository_id, '_build/java/connect.sdk-18.0-sources.jar')
-    upload(repository_id, '_build/java/connect.sdk-18.0-javadoc.jar')
+
+    for file in files:
+        fullname = '{}/{}'.format(path, file)
+        ascname = fullname + '.asc'
+        print('Uploading "{}"...'.format(fullname))
+        upload(repository_id, fullname)
+        print('Uploading "{}"...'.format(ascname))
+        upload(repository_id, ascname)
+    
+    print('Closing repository...')
     close()
+
+    print('Releasing repository...')
     release()
+
+    print('Done.')
