9b235d24d426f88513e975b7ac6261fec046c0af
Adrian <adrian.gonzalezalonso@ingrammicro.com>
2020-05-06 09:44:23 +0200

    Conflicts

--- test/unit/TestSuite.hx
+++ test/unit/TestSuite.hx
@@ -33,35 +33,35 @@ import FlowTest;
  */
 class TestSuite extends massive.munit.TestSuite
 {
-	public function new()
-	{
-		super();
+    public function new()
+    {
+        super();
 
-		add(LoggerTest);
-		add(MarketplaceTest);
-		add(TierAccountTest);
-		add(AssetTest);
-		add(MarkdownLoggerFormatterTest);
-		add(CategoryTest);
-		add(ItemTest);
-		add(ConfigurationTest);
-		add(CustomLoggerFormatterTest);
-		add(AccountTest);
-		add(TierConfigRequestTest);
-		add(DiffTest);
-		add(FlowAttemptsTest);
-		add(AgreementTest);
-		add(AssetRequestTest);
-		add(UsageFileTest);
-		add(TierConfigTest);
-		add(ConversationTest);
-		add(ListingRequestTest);
-		add(ConfigTest);
-		add(ListingTest);
-		add(ModelTest);
-		add(QueryTest);
-		add(ProductTest);
-		add(DateTimeTest);
+        add(LoggerTest);
+        add(MarketplaceTest);
+        add(TierAccountTest);
+        add(AssetTest);
+        add(MarkdownLoggerFormatterTest);
+        add(CategoryTest);
+        add(ItemTest);
+        add(ConfigurationTest);
+        add(CustomLoggerFormatterTest);
+        add(AccountTest);
+        add(TierConfigRequestTest);
+        add(DiffTest);
+        add(FlowAttemptsTest);
+        add(AgreementTest);
+        add(AssetRequestTest);
+        add(UsageFileTest);
+        add(TierConfigTest);
+        add(ConversationTest);
+        add(ListingRequestTest);
+        add(ConfigTest);
+        add(ListingTest);
+        add(ModelTest);
+        add(QueryTest);
+        add(ProductTest);
+        add(DateTimeTest);
         add(FlowTest);
-	}
+    }
 }
