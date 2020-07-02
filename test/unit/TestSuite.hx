e21033667f429e02518d5bf1860fb0175cf05216
Adrian <adrian.gonzalezalonso@ingrammicro.com>
2020-07-02 13:30:53 +0200

    Fix style

--- test/unit/TestSuite.hx
+++ test/unit/TestSuite.hx
@@ -33,34 +33,34 @@ import DateTimeTest;
  */
 class TestSuite extends massive.munit.TestSuite
 {
-	public function new()
-	{
-		super();
+    public function new()
+    {
+        super();
         add(LoggerTest);
-		add(MarketplaceTest);
-		add(TierAccountTest);
-		add(AssetTest);
-		add(MarkdownLoggerFormatterTest);
-		add(CategoryTest);
-		add(ItemTest);
-		add(ConfigurationTest);
+        add(MarketplaceTest);
+        add(TierAccountTest);
+        add(AssetTest);
+        add(MarkdownLoggerFormatterTest);
+        add(CategoryTest);
+        add(ItemTest);
+        add(ConfigurationTest);
         add(CustomLoggerFormatterTest);
-		add(AccountTest);
-		add(TierConfigRequestTest);
-		add(DiffTest);
+        add(AccountTest);
+        add(TierConfigRequestTest);
+        add(DiffTest);
         add(FlowAttemptsTest);
-		add(AgreementTest);
-		add(AssetRequestTest);
-		add(UsageFileTest);
-		add(TierConfigTest);
-		add(ConversationTest);
+        add(AgreementTest);
+        add(AssetRequestTest);
+        add(UsageFileTest);
+        add(TierConfigTest);
+        add(ConversationTest);
         add(FlowTest);
-		add(ListingRequestTest);
-		add(ConfigTest);
-		add(ListingTest);
-		add(ModelTest);
-		add(QueryTest);
-		add(ProductTest);
-		add(DateTimeTest);
-	}
+        add(ListingRequestTest);
+        add(ConfigTest);
+        add(ListingTest);
+        add(ModelTest);
+        add(QueryTest);
+        add(ProductTest);
+        add(DateTimeTest);
+    }
 }
