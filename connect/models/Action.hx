7e924c064d40ba09f28b3f87b4d0973382aa66f1
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-07-10 09:59:06 +0200

    Update Action.hx

--- connect/models/Action.hx
+++ connect/models/Action.hx
@@ -4,20 +4,22 @@
 */
 package connect.models;
 
-
 class Action extends IdModel {
-    /** Title field of Action. **/
-    public var title: String;
+    /** Name field of Action. **/
+    public var name: String;
 
+    /** The action field of the Action table **/
+    public var action: String;
 
     /** Only supported type is button for now. **/
     public var type: String;
 
-
     /** A description of the action, describing the operation that will be performed. **/
     public var description: String;
 
-
     /** Specifies all constrains applied on the action. One of: asset, tier-1, tier-2. **/
     public var scope: String;
+
+    /** Events occured on the action. **/
+    public var events: Events;
 }
