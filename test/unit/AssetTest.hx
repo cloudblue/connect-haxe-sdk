dcf65935b516a819e08863e3802ffd57c2c36b01
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-08-03 14:48:17 +0200

    Added Param.structuredValue.

--- test/unit/AssetTest.hx
+++ test/unit/AssetTest.hx
@@ -177,6 +177,8 @@ class AssetTest {
         Assert.areEqual('2019-08-27T14:21:23+00:00', itemParam.events.updated.at.toString());
         Assert.areEqual('UR-841-574-187', itemParam.events.updated.by.id);
         Assert.areEqual('Marc Serrat', itemParam.events.updated.by.name);
+        Assert.isTrue(itemParam.isCheckboxChecked());
+        Assert.isFalse(item.params.get(1).isCheckboxChecked());
 
         // Check mocks
         final apiMock = cast(Env.getFulfillmentApi(), Mock);
