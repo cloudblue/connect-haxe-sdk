8db835f9fa8075830870851321f541861c43e6bc
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-08-06 15:38:18 +0200

    Documented Subscription.

--- connect/models/Subscription.hx
+++ connect/models/Subscription.hx
@@ -7,6 +7,9 @@ package connect.models;
 import connect.api.Query;
 import connect.util.Collection;
 
+/**
+ * This class represents assets with one or more reservation type items.
+ */
 class Subscription extends IdModel {
     public var status: String;
     public var events: Events;
@@ -28,11 +31,40 @@ class Subscription extends IdModel {
         ]);
     }
 
+    /**
+        Lists all subscription assets that match the given filters. Supported filters are:
+
+        - `status`
+        - `events.created.at`
+        - `events.created.at`
+        - `billing.period.uom`
+        - `billing.next_date`
+        - `external_id`
+        - `external_uid`
+        - `product.id`
+        - `product.name`
+        - `connection.id`
+        - `connection.type`
+        - `connection.provider.id`
+        - `connection.provider.name`
+        - `connection.vendor.id`
+        - `connection.vendor.name`
+        - `connection.hub.id`
+        - `connection.hub.name`
+        - `marketplace.id`
+        - `marketplace.name`
+        - `tiers.customer.id`
+        - `tiers.tier1.id`
+        - `tiers.tier2.id`
+
+        @returns A Collection of Subscriptions.
+    **/
     public static function list(filters: Query): Collection<Subscription> {
         final subscriptions = Env.getSubscriptionsApi().listRecurringAssets(filters);
         return Model.parseArray(Subscription, subscriptions);
     }
 
+    /** @returns The Subscription with the given id, or `null` if it was not found. **/
     public static function get(id: String): Subscription {
         try {
             final subscription = Env.getSubscriptionsApi().getRecurringAsset(id);
