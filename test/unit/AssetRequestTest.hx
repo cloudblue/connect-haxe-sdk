e2f17188011842fd7c9eeadd8e0d08442ba67b91
Javier San Juan Cervera <javier.sanjuancervera@ingrammicro.com>
2020-08-12 13:37:58 +0200

    Updated AssetRequestTest

--- test/unit/AssetRequestTest.hx
+++ test/unit/AssetRequestTest.hx
@@ -2,6 +2,8 @@
     This file is part of the Ingram Micro CloudBlue Connect SDK.
     Copyright (c) 2019 Ingram Micro. All Rights Reserved.
 */
+import connect.api.IApiClient;
+import connect.api.Response;
 import connect.Env;
 import connect.models.Asset;
 import connect.models.AssetRequest;
@@ -10,24 +12,21 @@ import connect.models.Conversation;
 import connect.models.Marketplace;
 import connect.models.Model;
 import connect.models.Param;
+import connect.util.Blob;
 import connect.util.Collection;
 import connect.util.Dictionary;
 import massive.munit.Assert;
+import sys.io.File;
 import test.mocks.Mock;
 
 class AssetRequestTest {
-    /*
     @Before
     public function setup() {
-        Env._reset(new Dictionary()
-            .setString('IFulfillmentApi', 'test.mocks.FulfillmentApiMock')
-            .setString('IGeneralApi', 'test.mocks.GeneralApiMock'));
+        Env._reset(new AssetRequestApiClient());
     }
-
 
     @Test
     public function testList() {
-        // Check subject
         final requests = AssetRequest.list(null);
         Assert.isType(requests, Collection);
         Assert.areEqual(2, requests.length());
@@ -36,19 +35,10 @@ class AssetRequestTest {
         Assert.areEqual('PR-5852-1608-0001', requests.get(1).id);
         Assert.areEqual('ApiKey XXX', requests.get(0).assignee);
         Assert.areEqual(haxe.Json.stringify({id: 'XXX'}), requests.get(1).assignee);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('listRequests'));
-        Assert.areEqual(
-            [null].toString(),
-            apiMock.callArgs('listRequests', 0).toString());
     }
-
 
     @Test
     public function testGetOk() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         Assert.isType(request, AssetRequest);
         Assert.isType(request.asset, Asset);
@@ -67,83 +57,38 @@ class AssetRequestTest {
         Assert.areEqual('MP Spain', request.marketplace.name);
         Assert.areEqual('/media/PA-063-000/marketplaces/MP-77000/icon.svg', request.marketplace.icon);
         Assert.areEqual('ApiKey XXX', request.assignee);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('getRequest'));
-        Assert.areEqual(
-            ['PR-5852-1608-0000'].toString(),
-            apiMock.callArgs('getRequest', 0).toString());
     }
-
 
     @Test
     public function testGetKo() {
-        // Check subject
-        final request = AssetRequest.get('PR-XXXX-XXXX-XXXX');
-        Assert.isNull(request);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('getRequest'));
-        Assert.areEqual(
-            ['PR-XXXX-XXXX-XXXX'].toString(),
-            apiMock.callArgs('getRequest', 0).toString());
+        Assert.isNull(AssetRequest.get('PR-XXXX-XXXX-XXXX'));
     }
-
 
     @Test
     public function testRegister() {
-        // Check subject
         final request = new AssetRequest().register();
         Assert.isType(request, AssetRequest);
-        Assert.areEqual('PR-5852-1608-0000', request.id);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('createRequest'));
-        Assert.areEqual(
-            [new AssetRequest()].toString(),
-            apiMock.callArgs('createRequest', 0).toString());
     }
-
 
     @Test
     public function testUpdate() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         request.note = 'Hello, world!';
         final updatedRequest = request.update(null);
         Assert.isType(updatedRequest, AssetRequest);
-        Assert.areEqual(AssetRequest.get('PR-5852-1608-0000').toString(), updatedRequest.toString());
-        // ^ The mock returns that request
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('updateRequest'));
-        Assert.areEqual(
-            [request.id, request._toDiffString()].toString(),
-            apiMock.callArgs('updateRequest', 0).toString());
+        Assert.areNotEqual(updatedRequest, request);
     }
-
 
     @Test
     public function testUpdateNoChanges() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         final updatedRequest = request.update(null);
         Assert.isType(updatedRequest, AssetRequest);
-        Assert.areEqual(request.toString(), updatedRequest.toString());
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(0, apiMock.callCount('updateRequest'));
+        Assert.areEqual(updatedRequest, request);
     }
-
 
     @Test
     public function testUpdateWithParams() {
-        // Check subject
         final param = new Param();
         param.id = 'PM-9861-7949-8492-0001';
         param.valueError = 'Please provide a value.';
@@ -151,170 +96,142 @@ class AssetRequestTest {
         final updatedRequest = request.update(new Collection<Param>().push(param));
         Assert.isType(updatedRequest, AssetRequest);
         Assert.areEqual(request.toString(), updatedRequest.toString());
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('updateRequest'));
-        final callArgs = apiMock.callArgs('updateRequest', 0);
-        Assert.areEqual(
-            [request.id, '{"asset":{"params":[{"id":"PM-9861-7949-8492-0001","value_error":"Please provide a value."}]}}'].toString(),
-            [callArgs[0], Helper.sortStringObject(AssetRequest, callArgs[1])].toString());
     }
-
 
     @Test
     public function testApproveByTemplate() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         final approvedRequest = request.approveByTemplate('TL-XXX-XXX-XXX');
         Assert.isType(approvedRequest, AssetRequest);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('changeRequestStatus'));
-        Assert.areEqual(
-            ['PR-5852-1608-0000', 'approve', '{"template_id":"TL-XXX-XXX-XXX"}'].toString(),
-            apiMock.callArgs('changeRequestStatus', 0).toString());
     }
-
 
     @Test
     public function testApproveByTile() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         final approvedRequest = request.approveByTile('Hello, world!');
         Assert.isType(approvedRequest, AssetRequest);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('changeRequestStatus'));
-        Assert.areEqual(
-            ['PR-5852-1608-0000', 'approve', '{"activation_tile":"Hello, world!"}'].toString(),
-            apiMock.callArgs('changeRequestStatus', 0).toString());
     }
-
 
     @Test
     public function testFail() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         final failedRequest = request.fail("Failing...");
         Assert.isType(failedRequest, AssetRequest);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('changeRequestStatus'));
-        Assert.areEqual(
-            ['PR-5852-1608-0000', 'fail', '{"reason":"Failing..."}'].toString(),
-            apiMock.callArgs('changeRequestStatus', 0).toString());
     }
-
 
     @Test
     public function testInquire() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         final inquiredRequest = request.inquire(null);
         Assert.isType(inquiredRequest, AssetRequest);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('changeRequestStatus'));
-        Assert.areEqual(
-            ['PR-5852-1608-0000', 'inquire', '{}'].toString(),
-            apiMock.callArgs('changeRequestStatus', 0).toString());
     }
-
 
     @Test
     public function testInquireWithTemplate() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         final inquiredRequest = request.inquire("TL-000");
         Assert.isType(inquiredRequest, AssetRequest);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('changeRequestStatus'));
-        Assert.areEqual(
-            ['PR-5852-1608-0000', 'inquire', '{"template_id":"TL-000"}'].toString(),
-            apiMock.callArgs('changeRequestStatus', 0).toString());
     }
-
 
     @Test
     public function testPend() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         final pendedRequest = request.pend();
         Assert.isType(pendedRequest, AssetRequest);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('changeRequestStatus'));
-        Assert.areEqual(
-            ['PR-5852-1608-0000', 'pend', '{}'].toString(),
-            apiMock.callArgs('changeRequestStatus', 0).toString());
     }
-
 
     @Test
     public function testAssign() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         final assignedRequest = request.assign('XXX');
         Assert.isType(assignedRequest, AssetRequest);
         Assert.areEqual('XXX', assignedRequest.assignee);
-
-        // Check mocks
-        final apiMock = cast(Env.getFulfillmentApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('assignRequest'));
-        Assert.areEqual(
-            ['PR-5852-1608-0000', 'XXX'].toString(),
-            apiMock.callArgs('assignRequest', 0).toString());
     }
-
 
     @Test
     public function testNeedsMigrationFalse() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         Assert.isFalse(request.needsMigration());
     }
 
-
     @Test
     public function testNeedsMigrationFalse2() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         request.asset.params.push(Model.parse(Param, '{"id": "migration_info", "value": ""}'));
         Assert.isFalse(request.needsMigration());
     }
 
-
     @Test
     public function testNeedsMigrationTrue() {
-        // Check subject
         final request = AssetRequest.get('PR-5852-1608-0000');
         request.asset.params.push(Model.parse(Param, '{"id": "migration_info", "value": "..."}'));
         Assert.isTrue(request.needsMigration());
     }
 
-
     @Test
     public function testGetConversation() {
-        // Check subject
-        final request = AssetRequest.get('PR-5852-1608-0000');
+        final request = new AssetRequest();
+        request.id = 'PR-5852-1608-0001';
         final conv = request.getConversation();
         Assert.isType(conv, Conversation);
         Assert.areEqual('PR-5852-1608-0000', conv.instanceId);
-
-        // Check mocks
-        final apiMock = cast(Env.getGeneralApi(), Mock);
-        Assert.areEqual(1, apiMock.callCount('listConversations'));
-        Assert.areEqual(
-            ['?eq(instance_id,PR-5852-1608-0000)'].toString(),
-            apiMock.callArgs('listConversations', 0).toString());
     }
-    */
+}
+
+class AssetRequestApiClient extends Mock implements IApiClient {
+    static final FILE = 'test/unit/data/requests.json';
+    static final CONVERSATIONS_FILE = 'test/unit/data/conversations.json';
+
+    public function syncRequest(method: String, url: String, headers: Dictionary, body: String,
+            fileArg: String, fileName: String, fileContent: Blob, certificate: String) : Response {
+        this.calledFunction('syncRequest', [method, url, headers, body,
+            fileArg, fileName, fileContent, certificate]);
+        switch (method) {
+            case 'GET':
+                switch (url) {
+                    case 'https://api.conn.rocks/public/v1/requests':
+                        return new Response(200, File.getContent(FILE), null);
+                    case 'https://api.conn.rocks/public/v1/requests/PR-5852-1608-0000':
+                        final list = Mock.parseJsonFile(FILE);
+                        return new Response(200, haxe.Json.stringify(list[0]), null);
+                    case 'https://api.conn.rocks/public/v1/conversations?instance_id=PR-5852-1608-0000':
+                        return new Response(200, '[]', null);
+                    case 'https://api.conn.rocks/public/v1/conversations?instance_id=PR-5852-1608-0001':
+                        return new Response(200, File.getContent(CONVERSATIONS_FILE), null);
+                    case 'https://api.conn.rocks/public/v1/conversations/CO-000-000-000':
+                        final conv = Mock.parseJsonFile(CONVERSATIONS_FILE)[0];
+                        return new Response(200, haxe.Json.stringify(conv), null);
+                }
+            case 'POST':
+                switch (url) {
+                    case 'https://api.conn.rocks/public/v1/requests':
+                        return new Response(200, body, null);
+                    case 'https://api.conn.rocks/public/v1/requests/PR-5852-1608-0000/approve':
+                        final request = Mock.parseJsonFile(FILE)[0];
+                        request.status = 'approved';
+                        return new Response(200, haxe.Json.stringify(request), null);
+                    case 'https://api.conn.rocks/public/v1/requests/PR-5852-1608-0000/fail':
+                        final request = Mock.parseJsonFile(FILE)[0];
+                        request.status = 'failed';
+                        return new Response(200, haxe.Json.stringify(request), null);
+                    case 'https://api.conn.rocks/public/v1/requests/PR-5852-1608-0000/inquire':
+                        final request = Mock.parseJsonFile(FILE)[0];
+                        request.status = 'inquired';
+                        return new Response(200, haxe.Json.stringify(request), null);
+                    case 'https://api.conn.rocks/public/v1/requests/PR-5852-1608-0000/pend':
+                        final request = Mock.parseJsonFile(FILE)[0];
+                        request.status = 'pending';
+                        return new Response(200, haxe.Json.stringify(request), null);
+                    case 'https://api.conn.rocks/public/v1/requests/PR-5852-1608-0000/assign/XXX':
+                        final request = Mock.parseJsonFile(FILE)[0];
+                        request.assignee = 'XXX';
+                        return new Response(200, haxe.Json.stringify(request), null);
+                }
+            case 'PUT':
+                if (url == 'https://api.conn.rocks/public/v1/requests/PR-5852-1608-0000') {
+                    return new Response(200, body, null);
+                }
+        }
+        return new Response(404, null, null);
+    }
 }
