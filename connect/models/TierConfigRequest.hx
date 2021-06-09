813de7b8a304983ac8eaf481083584892425f0b4
JaviCerveraIngram <javier.sanjuancervera@ingrammicro.com>
2021-06-09 10:47:33 +0200

    Added requested feature

--- connect/models/TierConfigRequest.hx
+++ connect/models/TierConfigRequest.hx
@@ -154,15 +154,20 @@ class TierConfigRequest extends IdModel {
     **/
     public function update(params: Collection<Param>): TierConfigRequest {
         if (params == null) {
-            final diff = this._toDiff();
-            final hasModifiedFields = Reflect.fields(diff).length > 1;
-            if (hasModifiedFields) {
-                final request = Env.getTierApi().updateTierConfigRequest(
-                    this.id,
-                    prepareUpdateBody(diff), this);
-                return Model.parse(TierConfigRequest, request);
+            final modifiedParams = getModifiedTcrParams();
+            if (modifiedParams != null) {
+                return this.update(modifiedParams);
             } else {
-                return this;
+                final diff = this._toDiff();
+                final hasModifiedFields = Reflect.fields(diff).length > 1;
+                if (hasModifiedFields) {
+                    final request = Env.getTierApi().updateTierConfigRequest(
+                        this.id,
+                        prepareUpdateBody(diff), this);
+                    return Model.parse(TierConfigRequest, request);
+                } else {
+                    return this;
+                }
             }
         } else {
             if (params.length() > 0) {
@@ -171,6 +176,24 @@ class TierConfigRequest extends IdModel {
                     '{"params":${params.toString()}}', this);
             }
             return this;
+        }
+    }
+
+    // If the user has modified the TCR params directly, returns a collection with them.
+    // Otherwise, returns null and modified params should be searched for in TC.
+    private function getModifiedTcrParams(): Null<Collection<Param>> {
+        final oldTcr = Model.parse(TierConfigRequest, this._footprint);
+        if (this.params.length() != oldTcr.params.length()) {
+            final oldParamsAsString = oldTcr.params.toArray().map(p -> p.toString());
+            final result = new Collection<Param>();
+            for (param in this.params.toArray()) {
+                if (oldParamsAsString.indexOf(param.toString()) == -1) {
+                    result.push(param);
+                }
+            }
+            return result;
+        } else {
+            return null;
         }
     }
 
